503b64876a27cb0a757d7e209b56947730d33c45
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2023-04-21 11:39:06 -0400

    Import `@bedrock/web-wallet` in test.

--- test/web/10-api.js
+++ test/web/10-api.js
@@ -1,6 +1,10 @@
 /*!
  * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
  */
+import * as webwallet from '@bedrock/web-wallet';
+
+console.log(webwallet, 'webwallet');
+
 describe('FIXME API', function() {
   describe('FIXME API', function() {
     it('should test something', async () => {
