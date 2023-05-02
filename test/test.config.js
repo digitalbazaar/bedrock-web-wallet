a87f8aaa32ae64cf0d5938edce34b75cc17a3619
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2023-05-02 09:54:06 -0400

    Update test config to enable meter usage and authenticate test account.

--- test/test.config.js
+++ test/test.config.js
@@ -5,13 +5,6 @@ import {config} from '@bedrock/core';
 import {createRequire} from 'node:module';
 import {fileURLToPath} from 'url';
 import path from 'path';
-import '@bedrock/https-agent';
-import '@bedrock/karma';
-import '@bedrock/express';
-import '@bedrock/account-http';
-import '@bedrock/profile-http';
-import '@bedrock/session-http';
-import '@bedrock/mongodb';
 
 const require = createRequire(import.meta.url);
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -27,14 +20,32 @@ config.mocha.tests.push(path.join(__dirname, 'mocha'));
 
 // allow self-signed certs in test framework
 config['https-agent'].rejectUnauthorized = false;
+config['https-agent'].keepAlive = true;
+
+// do not require an authentication session for tests
+config['kms-http'].requireAuthentication = false;
+
+config.server.host = 'localhost:9876';
+config.kms.allowedHost = config.server.host;
+
+config.profile.kms.baseUrl = `${config.server.baseUri}/kms`;
+config.profile.kms.ipAllowList = ['127.0.0.1/32'];
+
+// do not require an authentication session for tests
+config['kms-http'].requireAuthentication = false;
+
+config.karma.suites['bedrock-web-wallet'] = path.join('web', '**', '*.js');
 
 // use session
 config.express.useSession = true;
+config.express.jsonErrorLevel = 'full';
 
-config.karma.suites['bedrock-web-wallet'] = path.join('web', '**', '*.js');
 config.karma.config.proxies = {
   '/': 'https://localhost:18443/'
 };
 config.karma.config.proxyValidateSSL = false;
 config.karma.config.webpack.resolve.fallback.events =
   require.resolve('events/');
+
+// do not fetch v1 dids from testnet
+config['did-io'].methodOverrides.v1.disableFetch = true;
