4781406ee87d3d1f7acf473d2cd21d0a3ad04ce6
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2023-05-02 11:51:59 -0400

    Fix assertions and move creation of unsigned presentation into helpers.

--- test/web/helpers.js
+++ test/web/helpers.js
@@ -1,6 +1,7 @@
 /*!
  * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
  */
+import * as vc from '@digitalbazaar/vc';
 import * as webWallet from '@bedrock/web-wallet';
 import {config} from '@bedrock/web';
 
@@ -44,17 +45,29 @@ export async function createProfile({name, email, accountId}) {
   });
 }
 
+export function createUnsignedPresentation({
+  profileId, presentationId, verifiableCredential
+}) {
+  // create an unsigned presentation
+  const unsignedPresentation = vc.createPresentation({
+    holder: profileId,
+    id: presentationId,
+    verifiableCredential
+  });
+  return unsignedPresentation;
+}
+
 export function assertSignedPresentation({
   signedPresentation, credential, presentationId, profileId, expectedProofType,
-  cryptosuite
+  expectedCryptosuite
 } = {}) {
   signedPresentation.verifiableCredential[0].should.eql(credential);
   signedPresentation.type.should.eql(['VerifiablePresentation']);
   signedPresentation.id.should.equal(presentationId);
   signedPresentation.holder.should.equal(profileId);
   signedPresentation.should.have.property('proof');
-  signedPresentation.proof.type.should.eql(expectedProofType);
-  if(cryptosuite) {
-    signedPresentation.proof.cryptosuite.should.eql(cryptosuite);
+  signedPresentation.proof.type.should.equal(expectedProofType);
+  if(expectedCryptosuite) {
+    signedPresentation.proof.cryptosuite.should.equal(expectedCryptosuite);
   }
 }
